224ebcd55c015663593be6c03cac172aa93e6272
koga yushi <youshe.old.pleasure@gmail.com>
2023-09-18 21:47:05 +0900

    initial commit

---
+++ src/main/java/kogayushi/deadletterqueue/DeadletterqueueApplication.java
@@ -0,0 +1,13 @@
+package kogayushi.deadletterqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeadletterqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeadletterqueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kogayushi/deadletterqueue/DeadletterqueueApplicationTests.java
@@ -0,0 +1,13 @@
+package kogayushi.deadletterqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeadletterqueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
